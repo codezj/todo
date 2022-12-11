72d9fb493426851b3b3edd6367f87077f9e20a36
codezj <42575555+codezj@users.noreply.github.com>
2022-12-11 18:29:21 +0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+  names = ['aaaa','bbbbb']
+  
+
+  private list = new TodoList("Bob",[
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[]{
+    // return this.list.items;
+    // return this.list.items.filter(item => !item.complete);
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem: any){
+    if (newItem != ""){
+      this.list.addItem(newItem);
+    }
+  }
+  
+  showComplete: boolean = false;
+}
